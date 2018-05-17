f11e16f9230436e9ad6583363fd0655dc987ee1e
Isma Gilani <igilani@renci.org>
2018-05-17 11:21:21 -0400

    minid and bdbag work

--- views.py
+++ views.py
@@ -12,11 +12,10 @@ from django.http import HttpResponse, FileResponse, HttpResponseRedirect
 from django.core.exceptions import PermissionDenied
 
 from hs_core.views.utils import authorize, ACTION_TO_AUTHORIZE
-from hs_core.tasks import create_bag_by_irods
 from hs_core.hydroshare.resource import FILE_SIZE_LIMIT
 from hs_core.signals import pre_download_file, pre_check_bag_flag
 from hs_core.hydroshare import check_resource_type
-from hs_core.hydroshare.hs_bagit import create_bag_files
+from hs_core.hydroshare import hs_bagit
 
 from . import models as m
 from .icommands import Session, GLOBAL_SESSION
@@ -42,96 +41,35 @@ def download(request, path, rest_call=False, use_async=True, *args, **kwargs):
             response.content = "<h1>" + content_msg + "</h1>"
             return response
 
-    if res.resource_federation_path:
-        # the resource is stored in federated zone
-        istorage = IrodsStorage('federated')
-        federated_path = res.resource_federation_path
-        path = os.path.join(federated_path, path)
+    istorage = IrodsStorage()
+
+    if 'environment' in kwargs:
+        environment = int(kwargs['environment'])
+        environment = m.RodsEnvironment.objects.get(pk=environment)
+        session = Session("/tmp/django_irods", settings.IRODS_ICOMMANDS_PATH,
+                          session_id=uuid4())
+        session.create_environment(environment)
+        session.run('iinit', None, environment.auth)
+    elif getattr(settings, 'IRODS_GLOBAL_SESSION', False):
+        session = GLOBAL_SESSION
+    elif icommands.ACTIVE_SESSION:
         session = icommands.ACTIVE_SESSION
     else:
-        istorage = IrodsStorage()
-        federated_path = ''
-        if 'environment' in kwargs:
-            environment = int(kwargs['environment'])
-            environment = m.RodsEnvironment.objects.get(pk=environment)
-            session = Session("/tmp/django_irods", settings.IRODS_ICOMMANDS_PATH,
-                              session_id=uuid4())
-            session.create_environment(environment)
-            session.run('iinit', None, environment.auth)
-        elif getattr(settings, 'IRODS_GLOBAL_SESSION', False):
-            session = GLOBAL_SESSION
-        elif icommands.ACTIVE_SESSION:
-            session = icommands.ACTIVE_SESSION
-        else:
-            raise KeyError('settings must have IRODS_GLOBAL_SESSION set '
-                           'if there is no environment object')
-
-    resource_cls = check_resource_type(res.resource_type)
-
-    if federated_path:
-        res_root = os.path.join(federated_path, res_id)
-    else:
-        res_root = res_id
-
-    bag_modified = "false"
-    metadata_dirty = "false"
-    if istorage.exists(res_root):
-        bag_modified = istorage.getAVU(res_root, 'bag_modified')
+        raise KeyError('settings must have IRODS_GLOBAL_SESSION set '
+                       'if there is no environment object')
+    if istorage.exists(res_id):
+        bag_modified = istorage.getAVU(res_id, 'bag_modified')
         # make sure if bag_modified is not set to true, we still recreate the bag if the
         # bag file does not exist for some reason to resolve the error to download a nonexistent
         # bag when bag_modified is false due to the flag being out-of-sync with the real bag status
         if bag_modified is None or bag_modified.lower() == "false":
             # check whether the bag file exists
             bag_file_name = res_id + '.zip'
-            if res_root.startswith(res_id):
-                bag_full_path = os.path.join('bags', bag_file_name)
-            else:
-                bag_full_path = os.path.join(federated_path, 'bags', bag_file_name)
-            # set bag_modified to 'true' if the bag does not exist so that it can be recreated
-            # and the bag_modified AVU will be set correctly as well subsequently
+            bag_full_path = os.path.join('bags', bag_file_name)
             if not istorage.exists(bag_full_path):
-                bag_modified = 'true'
-        metadata_dirty = istorage.getAVU(res_root, 'metadata_dirty')
+                hs_bagit.create_bdbag(res)
 
-    if is_bag_download:
-        # do on-demand bag creation
-        # needs to check whether res_id collection exists before getting/setting AVU on it
-        # to accommodate the case where the very same resource gets deleted by another request
-        # when it is getting downloaded
-
-        # send signal for pre_check_bag_flag
-        pre_check_bag_flag.send(sender=resource_cls, resource=res)
-        if bag_modified is None or bag_modified.lower() == "true":
-            if metadata_dirty is None or metadata_dirty.lower() == 'true':
-                create_bag_files(res)
-            if use_async:
-                # task parameter has to be passed in as a tuple or list, hence (res_id,) is needed
-                # Note that since we are using JSON for task parameter serialization, no complex
-                # object can be passed as parameters to a celery task
-                task = create_bag_by_irods.apply_async((res_id,), countdown=3)
-                if rest_call:
-                    return HttpResponse(json.dumps({'bag_status': 'Not ready',
-                                                    'task_id': task.task_id}),
-                                        content_type="application/json")
-
-                request.session['task_id'] = task.task_id
-                request.session['download_path'] = request.path
-                return HttpResponseRedirect(res.get_absolute_url())
-            else:
-                ret_status = create_bag_by_irods(res_id)
-                if not ret_status:
-                    content_msg = "Bag cannot be created successfully. Check log for details."
-                    response = HttpResponse()
-                    if rest_call:
-                        response.content = content_msg
-                    else:
-                        response.content = "<h1>" + content_msg + "</h1>"
-                    return response
-
-    elif metadata_dirty is None or metadata_dirty.lower() == 'true':
-        if path.endswith("resourcemap.xml") or path.endswith('resourcemetadata.xml'):
-            # we need to regenerate the metadata xml files
-            create_bag_files(res)
+    resource_cls = check_resource_type(res.resource_type)
 
     # send signal for pre download file
     download_file_name = split_path_strs[-1]
@@ -152,12 +90,12 @@ def download(request, path, rest_call=False, use_async=True, *args, **kwargs):
         proc = session.run_safe('iget', None, path, *options)
         response = FileResponse(proc.stdout, content_type=mtype)
         response['Content-Disposition'] = 'attachment; filename="{name}"'.format(
-                                                        name=path.split('/')[-1])
+            name=path.split('/')[-1])
         response['Content-Length'] = flen
         return response
     else:
         content_msg = "File larger than 1GB cannot be downloaded directly via HTTP. " \
-                       "Please download the large file via iRODS clients."
+                      "Please download the large file via iRODS clients."
         response = HttpResponse(status=403)
         if rest_call:
             response.content = content_msg
